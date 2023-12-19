f501574249c83626454c8c4e76a9ed56d3d5750b
santypiff <153444870+santypiff@users.noreply.github.com>
2023-12-19 11:12:09 +0000

    .

--- w1/v1/theme.config.tsx
+++ w1/v1/theme.config.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>NoIsrl</span>,
+  logo: <span>𝕟𝕠𝕚𝕤𝕣𝕝</span>,
   docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: "From the river to the sea, Palestine will be free",
+    text: "𝒻𝓇𝑜𝓂 𝓉𝒽𝑒 𝓇𝒾𝓋𝑒𝓇 𝓉𝑜𝓉 𝒽𝑒 𝓈𝑒𝒶 𝓅𝒶𝓁𝑒𝓈𝓉𝒾𝓃𝑒 𝓌𝒾𝓁𝓁 𝒷𝑒 𝒻𝓇𝑒𝑒",
   },
 };
 

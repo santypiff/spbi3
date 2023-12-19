31745daf282f6251d43108c014e51562e8770832
santypiff <153444870+santypiff@users.noreply.github.com>
2023-12-19 11:26:15 +0000

    .

--- w1/v1/theme.config.tsx
+++ w1/v1/theme.config.tsx
@@ -4,6 +4,18 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 const config: DocsThemeConfig = {
   logo: <span>𝕟𝕠𝕚𝕤𝕣𝕝</span>,
   docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Nextra" />
+      <meta property="og:description" content="The next site builder" />
+      <link
+        rel="icon"
+        href="https://i.ibb.co/ZMdj0Ks/u-http-www-israelandstuff-com-wp-content-uploads-2015-09-Boycott-Israel.png"
+        type="image/x-icon"
+      />
+    </>
+  ),
   footer: {
     text: "𝒻𝓇𝑜𝓂 𝓉𝒽𝑒 𝓇𝒾𝓋𝑒𝓇 𝓉𝑜𝓉 𝒽𝑒 𝓈𝑒𝒶 𝓅𝒶𝓁𝑒𝓈𝓉𝒾𝓃𝑒 𝓌𝒾𝓁𝓁 𝒷𝑒 𝒻𝓇𝑒𝑒",
   },

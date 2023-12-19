69f28f409b1e399afa37f9e224a400f7bc15f87f
santypiff <153444870+santypiff@users.noreply.github.com>
2023-12-19 20:05:42 +0000

    .

--- w1/v1/next.config.js
+++ w1/v1/next.config.js
@@ -1,6 +1,17 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  images: {
+    domains: ["i.ibb.co"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+        port: "",
+        pathname: "/3BQWK09/**",
+      },
+    ],
+  },
+});
 
-module.exports = withNextra()
+module.exports = withNextra();

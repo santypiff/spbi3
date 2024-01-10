d8ccfa1a9e0821c9c79c3dc5d8101075cb9e7ad5
santypiff <153444870+santypiff@users.noreply.github.com>
2024-01-10 16:40:08 +0000

    .

---
+++ w1/v1/components/Tase.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect } from "react";
+
+const TradingViewWidget: React.FC = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src =
+      "https://s3.tradingview.com/external-embedding/embed-widget-hotlists.js";
+    script.async = true;
+    script.innerHTML = JSON.stringify({
+      colorTheme: "dark",
+      dateRange: "12M",
+      exchange: "TASE",
+      showChart: true,
+      locale: "en",
+      largeChartUrl: "",
+      isTransparent: false,
+      showSymbolLogo: true,
+      showFloatingTooltip: false,
+      width: "600",
+      height: "600",
+      plotLineColorGrowing: "rgba(41, 98, 255, 1)",
+      plotLineColorFalling: "rgba(41, 98, 255, 1)",
+      gridLineColor: "rgba(240, 243, 250, 0)",
+      scaleFontColor: "rgba(106, 109, 120, 1)",
+      belowLineFillColorGrowing: "rgba(41, 98, 255, 0.12)",
+      belowLineFillColorFalling: "rgba(41, 98, 255, 0.12)",
+      belowLineFillColorGrowingBottom: "rgba(41, 98, 255, 0)",
+      belowLineFillColorFallingBottom: "rgba(41, 98, 255, 0)",
+      symbolActiveColor: "rgba(41, 98, 255, 0.12)",
+    });
+
+    const container = document.getElementsByClassName(
+      "tradingview-widget-container"
+    )[0];
+    container.appendChild(script);
+
+    return () => {
+      container.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div className="tradingview-widget-container">
+      <div className="tradingview-widget-container__widget"></div>
+      <div className="tradingview-widget-copyright">
+        <a
+          href="https://www.tradingview.com/"
+          rel="noopener nofollow"
+          target="_blank"
+        >
+          <span className="blue-text">Track all markets on TradingView</span>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default TradingViewWidget;
